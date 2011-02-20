3d459bf5db8ad0e600be90ea43fbf9a612203bd3
crapooze <crapooze@gmail.com>
2011-02-20 14:21:12 +0100

    added a fallback to ResourceView for resources

--- lib/dope/core/resource.rb
+++ lib/dope/core/resource.rb
@@ -2,12 +2,13 @@
 require 'welo'
 
 module Dope
-  autoload :View, 'dope/core/view'
+  autoload :ResourceView, 'dope/core/view'
   module Resource
     include Welo::Resource
     def self.included(mod)
       mod.extend Welo::Resource::ClassMethods
       mod.extend ClassMethods
+      mod.view(ResourceView)
     end
 
     module ClassMethods
--- lib/dope/core/view.rb
+++ lib/dope/core/view.rb
@@ -1,6 +1,12 @@
 
 module Dope
   class View
+    def render(ctx)
+      "hello from #{self} in #{ctx}"
+    end
+  end
+
+  class ResourceView < View
     attr_reader :resource
     def initialize(resource)
       @resource = resource
